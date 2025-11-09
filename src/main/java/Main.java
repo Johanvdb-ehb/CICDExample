8e73e2e2887b9b8988bd4b6dd9c727980806f803
Johanvdb-ehb <johan.van.den.broek@ehb.be>
2025-11-09 19:38:08 +0100

    main aanpassing, writing dirs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
-
 
 		System.out.println("Hello, GitHub Actions with CI/CD and Pages!");
 
 		String message = "Hello, GitHub Actions with CI/CD and Pages!";
-
 		System.out.println(message);
+
+		File dir = new File("target/site");
+		if (!dir.exists()) {
+			// create directories target/ and target/site if they don't exist
+			dir.mkdirs();
+		}
 
 		try (FileWriter writer = new FileWriter("target/site/index.html")) {
 			writer.write("<html><body><h1>" + message + "</h1></body></html>");
