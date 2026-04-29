0923a881f18094914ed01984b3ee208a27ee2d51
Johanvdb-ehb <johan.van.den.broek@ehb.be>
2026-04-29 10:00:43 +0200

    kleine aanpassing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("Hello, GitHub Actions with CI/CD and Pages!");
-
 		String message = "Hello, GitHub Actions! with CI/CD and Pages!!";
 		System.out.println(message);
 
